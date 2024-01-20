803e69b504d9837d62190e11cbfa86675f9a95ef
Lucas Martins <lucasmartinsemp794@gmail.com>
2024-01-20 18:49:49 -0300

    Calculadora em C#

---
+++ Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace Calculator {
+    class Program {
+        static void Main(string [] args) {
+            Menu();
+        }
+ 
+        static void Menu() {
+            Console.Clear();
+
+            Console.WriteLine("Escola a opção desejada: ");
+            Console.WriteLine("1 - Somar");
+            Console.WriteLine("2 - Subtrair ");
+            Console.WriteLine("3 - Dividir ");
+            Console.WriteLine("4 - Multiplicar ");
+            Console.WriteLine("5 - Sair ");
+
+            Console.WriteLine("-------------------------");
+
+            Console.Write("Selecione uma opção: ");
+            short res = short.Parse(Console.ReadLine());
+            
+            switch(res) {
+                case 1: 
+                    Somar();
+                break;
+                case 2: 
+                    Subtrair();
+                break;
+                case 3:
+                    Dividir();
+                break;
+                case 4: 
+                    Multiplicar();
+                break;
+                case 5:
+                    Sair();
+                break;
+                default:
+                    Menu();
+                break;
+            }
+
+
+        }
+        static void Somar() {
+            Console.Clear();
+
+            Console.Write("Primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+            
+            Console.Write("Segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            
+            float resultado = valor1 + valor2;
+
+            Console.WriteLine($"O resultado da soma é: {resultado}");
+
+            Console.ReadKey();
+
+            Menu();
+        }
+ 
+        static void Subtrair() {
+            Console.Clear();
+
+            Console.Write("Primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = valor1 - valor2;
+
+            Console.WriteLine($"O resultado da subtração é: {resultado}");
+            
+            Console.ReadKey();
+
+            Menu();
+
+        }
+    
+        static void Dividir() {
+            Console.Clear();
+
+            Console.Write("Primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+            Console.Write("Segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = valor1 / valor2;
+
+            Console.WriteLine($"O resultado da divisão é: {resultado}");
+
+            Console.ReadKey();
+
+            Menu();
+        }
+ 
+        static void Multiplicar() {
+            Console.Clear();
+
+            Console.Write("Primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+            Console.Write("Segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+
+            float resultado = valor1 * valor2;
+
+            Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+
+            Console.ReadKey();
+ 
+            Menu();
+        }
+    
+        static void Sair() {
+            Console.Clear();
+            Console.WriteLine("Obrigado por utilizar meu programa :)");
+            System.Environment.Exit(0);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+<div align = "center">
+    <h1>Calculadora Básica em .NET C#</h1>
+</div>
+
+### 🖥 Sobre o Projeto
+- Aplicação desenvolvida para praticar a sintaxe do **.NET C#**;
+- Desenvolvi uma calculadora com todas as suas funções básicas, deleguei todas as operações em **Funções**, criei um Menu para inicializar o programa e uma Função para Saída;
+- Aplicação para console.
+  
+### 🌟 Tecnologias utilizadas
+- [.NET C#](https://learn.microsoft.com/pt-br/dotnet/csharp/)
+
+### 🎯 Imagens do projeto
+![image](https://github.com/LucasBrito794/miniblog/assets/78000311/3d34668a-be37-4caf-95fc-651ec66a0e4c)
+
+## 
+
+![image](https://github.com/LucasBrito794/miniblog/assets/78000311/7edf610e-0af7-41f2-97e5-30e3927583e7)
